cd6abf48091f2d1cac3eb006c99c350999527265
Lincianyong <110042648+Lincianyong@users.noreply.github.com>
2024-06-07 21:31:47 +0700

    last push

--- components/categoriesMenu.tsx
+++ components/categoriesMenu.tsx
@@ -1,32 +1,41 @@
-import {ICategory} from 'boundless-api-client';
-import {apiClient, revalidate} from '@/lib/api';
-import Link from 'next/link';
+import { ICategory } from "boundless-api-client";
+import { apiClient, revalidate } from "@/lib/api";
+import Link from "next/link";
 
 export default async function CategoriesMenu() {
-	const categories = await fetchCategoriesMenu();
+  const categories = await fetchCategoriesMenu();
 
-	return (
-		<div className={'container mb-4'}>
-			<ul className={'list-unstyled d-flex flex-wrap justify-content-center border-top border-bottom py-3 m-0'}
-					style={{gap: '15px'}}
-			>
-				{categories.map(({category_id, title, url_key}) =>
-					<li key={category_id}>
-						<Link href={`/collections/${url_key || category_id}`} className={'fs-4'}>{title}</Link>
-					</li>
-				)}
-			</ul>
-		</div>
-	);
+  return (
+    <div className={"container mb-4"}>
+      <ul
+        className={"d-flex flex-wrap justify-content-center m-0"}
+        style={{ gap: "15px" }}
+      >
+        {categories.map(({ category_id, title, url_key }) => (
+          <div key={category_id} className="flex">
+            <Link
+              href={`/collections/${url_key || category_id}`}
+              className={"fs-4"}
+            >
+              <p className="bg-slate-300 rounded-full px-6 p-2">{title}</p>
+            </Link>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 const fetchCategoriesMenu = async (): Promise<ICategory[]> => {
-	return apiClient.catalog.getCategoryTree({
-		menu: 'category'
-	}, {
-		next: {
-			revalidate,
-			tags: ['categories']
-		}
-	});
+  return apiClient.catalog.getCategoryTree(
+    {
+      menu: "category",
+    },
+    {
+      next: {
+        revalidate,
+        tags: ["categories"],
+      },
+    }
+  );
 };
